f49a3846b8bbe3cf483d62051ad38fa24b3acbe1
SATHILIN <sathilin@ford.com>
2024-09-04 15:18:49 +0530

    Jenkins app

--- src/main/java/com/jenkins/app/JenkinsApp3Application.java
+++ src/main/java/com/jenkins/app/JenkinsApp3Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsApp3Application {
 
 	public static void main(String[] args) {
+		System.out.println("--> My Applcation Being Started...");
 		SpringApplication.run(JenkinsApp3Application.class, args);
 	}
 
